adcf9095525bc8aa9d106b91820a13709beb51c2
Angold-4 <wangold4w@gmail.com>
2022-01-30 23:05:53 +0800

    a workable version

--- src/App.vue
+++ src/App.vue
@@ -19,15 +19,6 @@ export default {
 </script>
 
 <style>
-html, body {
-    height: 100%;
-    margin: 0;
-}
-
-#app {
-    font-family: "Helvetica Neue", Helvetica;
-    height: 100%;
-}
 
 .el-header {
     border-bottom: solid 1px #e6e6e6;
--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,4 +1,11 @@
 <template>
+    <div id="RankGroup" style="height: 2rem; white-space: nowrap">
+	<el-checkbox label="Rank" size="mini" v-model="RankColVisible"></el-checkbox>
+	<el-checkbox label="Short Exposure" size="mini" v-model="SEColVisible"></el-checkbox>
+	<el-checkbox label="Net Exposure" size="mini" v-model="NEColVisible"></el-checkbox>
+	<el-checkbox label="Cash Bal" size="mini" v-model="CBColVisible"></el-checkbox>
+	<el-checkbox label="Loan Bal" size="mini" v-model="LBColVisible"></el-checkbox>
+    <div>
     <el-table
 	id="RankTable"
 	:data="studentData"
@@ -6,26 +13,88 @@
 	:row-style="{height: '30px'}"
 	:cell-style="{padding: '0'}"
 	:header-row-style="{height: '40px'}"
-	:header-cell-style="{padding: '0px'}"
+	:header-cell-style="{padding: '10px'}"
 	stripe
-	height="calc(100% - 1.8rem)">
-	<el-table-column
-		align="center"
-		prop="Acct"
-		label="Acct#"
-		width="60">
+	height="calc(100% - 1.8rem)"
+	:default-sort = "{prop: 'RET', order: 'descending'}">
+	<el-table-column 
+	    type="index"
+	    v-if:"RankColVisible">
 	</el-table-column>
+	<el-table-column
+	    align="center"
+	    prop="Acct"
+	    label="Account#"
+	    width="120">
+	</el-table-column>
+	<el-table-column
+	    align="center"
+	    prop="SSE"
+	    label="Short Exposure"
+	    width="200"
+	    sortable
+	    v-if="SEColVisible">
+	</el-table-column>
+	<el-table-column
+	    align="center"
+	    prop="SNE"
+	    label="Net Exposure"
+	    width="200"
+	    sortable
+	    v-if="NEColVisible">
+	</el-table-column>
+	<el-table-column
+	    align="center"
+	    prop="CAB"
+	    label="Cash Bal"
+	    width="150"
+	    sortable
+	    v-if="CBColVisible">
+	</el-table-column>
+	<el-table-column
+	    align="center"
+	    prop="MLB"
+	    label="Loan Bal"
+	    width="140"
+	    sortable
+	    v-if="LBColVisible">
+	</el-table-column>
+	<el-table-column
+	    align="center"
+	    prop="AUM"
+	    label="AuM"
+	    width="200"
+	    sortable>
+	</el-table-column>
+	<el-table-column
+	    align="center"
+	    prop="RET"
+	    label="YTD Return"
+	    width="230"
+	    sortable>
+	    <template v-slot="scope">
+		<span :style="`color: #${scope.row.RET >= 0 ? '00FF00' : 'FF0000'}`">
+		    {{ scope.row.RET }}
+		</span>
+	</el-table-column>
+
+
     </el-table>
+    </div>
 </template>
 
 <script>
 import XLSX from 'xlsx-style';
-
 // :default-sort = "{prop: 'ytd-return', order: 'ascending'}">
 export default {
     name: 'RankTable',
     data() {
 	return {
+	    RankColVisible: true,
+	    NEColVisible: false,
+	    SEColVisible: false,
+	    LBColVisible: false,
+	    CBColVisible: false,
 	    studentData: []
 	}
 
@@ -47,9 +116,14 @@ export default {
 
 	    // read each student
 	    for (let i = firstEntryRow; ws[`A${i}`] !== undefined; ++i) {
-		console.log(ws[`A${i}`].v);
 		let student = {
-		    Acct: ws[`A${i}`].v
+		    Acct: ws[`A${i}`].v,
+		    SSE: ws[`B${i}`].v,
+		    SNE: ws[`C${i}`].v,
+		    CAB: ws[`D${i}`].v,
+		    MLB: ws[`E${i}`].v,
+		    AUM: ws[`F${i}`].v,
+		    RET: ws[`G${i}`].v
 		};
 		console.log(student);
 		this.studentData.push(student);
